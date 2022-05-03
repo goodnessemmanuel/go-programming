08b531bf98d0f3f35b3a53d8829c3403a06cd6c2
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-03 18:52:54 +0200

    data types

--- datatypes/datatypes.go
+++ datatypes/datatypes.go
@@ -1,16 +1,26 @@
 package datatypes
 
 import "fmt"
+
 //Long declaration
-var y = 42;
+var y = 42 //package scope decalared variable. similar to var z and n below
 //Decalared the variable with the Identifier "z" and is of type string
-var z string = "I am a variable of type string";
+// both z and n are referred to as string literrals
+var z string = "I am a variable of type string"
+var n string = `"I am also a variable of type string"`
+
+var status bool = true
+
+
 //End long variable declaration
 
- func Datatypes() string {
-    fmt.Println(y)
-    fmt.Printf("%T\n", y) //print the variable type
-    fmt.Println(z)
-    fmt.Printf("%T\n", z)
-    return z
+func Datatypes() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y) //print the variable type
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+	fmt.Println(n)
+	fmt.Printf("%T\n", n)
+    fmt.Println(status)
+	fmt.Printf("%T\n", status)
 }
