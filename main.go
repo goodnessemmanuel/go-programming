4950b6a1d5eeb104ac0ce9c3111851d419ebfd0f
Oche Ejeh <oeejeh@unionbankng.com>
2022-05-04 11:58:01 +0200

    string operation

--- main.go
+++ main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to go!")
+	fmt.Println(stringManipulate())
+}
+
+var testString string = `"this is a sample for testing"'`
+
+func stringManipulate() string {
+	maniPulateTest := []byte(testString) //string converted to byte sequence
+
+	//print the UTF-8 code point for maniPulateTest
+	for i := 0; i < len(maniPulateTest); i++ {
+		fmt.Printf("%d utf code point -> %#U \n", maniPulateTest[i], maniPulateTest[i])
+	}
+	fmt.Println()
+	return fmt.Sprintf("%v", maniPulateTest)
 }
